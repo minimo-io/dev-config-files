b7e39b112a3db27d55600557296126628f95558b
Nicolas Erramuspe <nicolas@minimo.io>
2025-11-10 12:36:44 -0300

    nvim configs

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,15 @@
+-- return {
+--   {
+--     "folke/noice.nvim",
+--     opts = {
+--       cmdline = {
+--         enabled = false,
+--       },
+--     },
+--   },
+-- }
+
+return {
+    "folke/noice.nvim",
+    enabled = false,
+}
